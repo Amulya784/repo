001450fd24c1c605ce08f0229ac7b70519497aad
Amulya784 <amulyagowda962@gmail.com>
2025-12-05 14:18:44 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,18 @@
+package sum;
+
+public class Sumofelemen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 2, 3, 4, 5};
+		int sum = 0;
+
+		for (int num : arr) {
+		    sum += num;
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
